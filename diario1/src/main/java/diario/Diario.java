51addb79f7338109413843b5b94bb6bb58c1b1c2
AFmunozz <146137698+AFmunozz@users.noreply.github.com>
2024-02-26 23:28:53 -0500

    Add files via upload

---
+++ diario1/src/main/java/diario/Diario.java
@@ -0,0 +1,21 @@
+/*
+@ author andres muñoz
+*/
+package diario;
+
+
+import umariana.diario.Entrada;
+
+
+public class Diario {
+
+  
+    public static void main(String[] args) {
+            Entrada e = new Entrada();
+       
+    
+        e.mostrarMenu();
+        
+        
+    }
+        }
